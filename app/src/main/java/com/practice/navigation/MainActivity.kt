23a893ef51468ba40852d4efc08220aa5df52017
GuidoDSalem <guidosalem.dev@gmail.com>
2022-07-25 17:31:58 -0300

    Initial commit

---
+++ app/src/main/java/com/practice/navigation/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.practice.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestinationDsl
+import com.practice.navigation.ui.theme.NavigationTheme
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.spec.NavGraphSpec
+import com.practice.navigation.destinations.*
+import java.time.LocalDateTime
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                    DestinationsNavHost(navGraph = NavGraphs.root)
+                }
+            }
+        }
+    }
+}
+
+@Destination(start = true)
+@Composable
+fun LoginScreen(
+    navigator: DestinationsNavigator
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Login Screen")
+        Button(onClick = {
+            navigator.navigate(ProfileScreenDestination(
+                user = User(
+                    name = "Guido",
+                    id = "1",
+                    created =  LocalDateTime.now()
+                )
+            ))
+        }) {
+            Text("Go to Profile Screen")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun ProfileScreen(
+    navigator: DestinationsNavigator,
+    user: User
+) {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Profile Screen: $user", textAlign = TextAlign.Center)
+        Button(onClick = {
+            navigator.navigate(PostScreenDestination())
+        }) {
+            Text("Go to Post Screen")
+        }
+    }
+}
+
+@Destination
+@Composable
+fun PostScreen(
+    navigator: DestinationsNavigator,
+    showOnlyPostsByUser: Boolean = false
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Post Screen, $showOnlyPostsByUser")
+    }
+}
